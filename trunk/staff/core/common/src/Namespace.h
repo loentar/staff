d539c49049627023c3b408557d79d82ee4bd54ef
loentar <loentar@46775792-f812-11dd-b507-8b4ecd5210be>
2009-06-05 07:10:44 +0000

    issue http://code.google.com/p/staff/issues/detail?id=2 Added staff::CNamespace class: wrapper for axiom_namespace_t Added staff::CDataObject namespace related functions

---
+++ trunk/staff/core/common/src/Namespace.h
@@ -0,0 +1,133 @@
+#ifndef _NAMESPACE_H_
+#define _NAMESPACE_H_
+
+#include "staffcommonexport.h"
+#include <string>
+
+typedef struct axiom_namespace axiom_namespace_t;
+
+namespace staff
+{
+  class CDataObject;
+
+  //! Namespace
+  class STAFF_COMMON_EXPORT CNamespace
+  {
+  public:
+    //!         default constructor
+    CNamespace();
+
+    //!         owner pass constructor
+    /*!         owner flag moved from rNamespace,
+                rNamespace's owner flag will be reset
+        \param  rNamespace - existing namespace
+        */
+    CNamespace(CNamespace& rNamespace);
+
+    //!         initializing constructor (bound to dataobject or not)
+    /*          
+        \param  pAxiomNamespace - existing axiom namespace
+        \param  pDataObject - bound dataobject or NULL
+        \param  bOwner - owner flag (if set, namespace will be deleted while object destructs)
+        */
+    CNamespace(axiom_namespace_t* pAxiomNamespace, CDataObject* pDataObject = NULL, bool bOwner = false);
+
+    //!         initializing constructor
+    /*! \param  sUri - namespace URI
+        \param  sPrefix - namespace prefix
+        */
+    CNamespace(const std::string& sUri, const std::string& sPrefix = "");
+
+    //!         attach existing axiom namespace to object
+    /*! \param  pAxiomNamespace - existing axiom namespace
+        \param  pDataObject - bound dataobject or NULL
+        \param  bOwner - owner flag
+        */
+    void Attach(axiom_namespace_t* pAxiomNamespace, CDataObject* pDataObject = NULL, bool bOwner = false);
+
+    //!         detach (and free namespace if owner)
+    void Detach();
+
+    //!         free namespace
+    void Free();
+
+    //!         clone namespace
+    /*! \return cloned namespace
+    */
+    CNamespace Clone() const;
+
+    //!         clone namespace
+    /*! \param  rNamespace - cloned namespace
+    */
+    const CNamespace& Clone(CNamespace& rNamespace) const;
+
+    //!         get prefix
+    /*! \return namespace prefix
+    */
+    std::string GetPrefix() const;
+
+    //!         get URI
+    /*! \return namespace URI
+    */
+    std::string GetUri() const;
+
+    //!         get pointer to bound dataobject
+    /*! \return get pointer to bound
+    */
+    CDataObject* GetDataObject() const;
+
+    //!         set pointer to bound dataobject
+    /*! resets owner flag
+        \return set pointer to bound
+    */
+    void SetDataObject(CDataObject* pDataObject);
+
+    //!         is owner
+    /*! \return true if object is owner
+    */
+    bool IsOwner() const;
+
+    //!         set object as owner
+    /*! \param  bOwner - true: set a owner, false: reset owner flag
+        */
+    void SetOwner(bool bOwner);
+
+    //!         test initialization
+    /*! \return true, if object is not initialized
+    */
+    bool IsNull();
+
+    //!         owner pass operator
+    /*! \param  rNamespace - source namespace
+        \return ref to current namespace
+        */
+    CNamespace& operator=(CNamespace& rNamespace);
+
+    //!         equality check
+    /*! \param  rNamespace - namespace to check with
+        \return true - namespaces is equal
+        */
+    bool operator==(const CNamespace& rNamespace) const;
+
+    //!         non equality check
+    /*! \param  rNamespace - namespace to check with
+        \return true - namespaces is not equal
+        */
+    bool operator!=(const CNamespace& rNamespace) const;
+
+    //!         operator for conversion to axiom_namespace_t
+    operator axiom_namespace_t*();
+
+    CNamespace* operator->();
+    const CNamespace* operator->() const;
+
+  private:
+    bool m_bOwner;
+    CDataObject* m_pDataObject;
+    axiom_namespace_t* m_pAxiomNamespace;
+    static axutil_env_t* m_pEnv;
+  };
+
+} // namespace staff
+
+#endif // _NAMESPACE_H_
