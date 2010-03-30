0a3ead02eb7b5f453f37b28e85bb64e2303d6c80
loentar <loentar@46775792-f812-11dd-b507-8b4ecd5210be>
2010-03-30 11:20:17 +0000

    fixed wrongly changed sqlite3 license header

--- trunk/staff/core/security/src/sqlite3.c
+++ trunk/staff/core/security/src/sqlite3.c
@@ -1,24 +1,3 @@
-/*
- *  Copyright 2010 Utkin Dmitry
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
-/*
- *  This file is part of the WSF Staff project.
- *  Please, visit http://code.google.com/p/staff for more information.
- */
-
 /******************************************************************************
 ** This file is an amalgamation of many separate C source files from SQLite
 ** version 3.6.22.  By combining all the individual C code files into this 
