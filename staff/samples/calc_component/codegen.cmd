4543d61e480a79d06532f75ec87e200adf2a8cb7
loentar <loentar@46775792-f812-11dd-b507-8b4ecd5210be>
2010-05-28 12:06:40 +0000

    windows: generating and installing wsdlfiles while building project

--- staff/samples/calc_component/codegen.cmd
+++ staff/samples/calc_component/codegen.cmd
@@ -2,4 +2,5 @@ setlocal
 PATH=%PATH%;..\..\..\..\rise\deploy\win_%PROCESSOR_ARCHITECTURE%\lib;..\..\deploy\win_%PROCESSOR_ARCHITECTURE%\bin
 cd src
 staff_codegen -u -tcomponent CalcService.h SubService.h
+staff_codegen -u -twsdl CalcService.h SubService.h
 endlocal
