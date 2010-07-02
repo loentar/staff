eb4b5518873aa254490eec727912567a69bfefea
loentar <loentar@46775792-f812-11dd-b507-8b4ecd5210be>
2010-07-02 10:24:04 +0000

    Added Windows projects and scripts for last changes

---
+++ trunk/staff/samples/echo/client_blocking/codegen.cmd
@@ -0,0 +1,6 @@
+setlocal
+SET STAFF_HOME=%cd%\..\..\..\deploy\win_%PROCESSOR_ARCHITECTURE%\staff
+set PATH=..\..\..\..\..\rise\deploy\win_%PROCESSOR_ARCHITECTURE%\lib;..\..\..\..\deploy\win_%PROCESSOR_ARCHITECTURE%\bin;%STAFF_HOME%\bin;%PATH%
+cd src
+staff_codegen -u -tclient Echo.h
+endlocal
---
+++ trunk/staff/samples/echo/client_blocking/deploy.cmd
@@ -0,0 +1,13 @@
+set projectname=echo_client_blocking
+
+set target=%1%
+if "%target%" == "" (
+  echo.
+  echo Error: Target is not set.
+  exit 1
+)
+
+set deploydir=%cd%\..\..\..\deploy\win_%PROCESSOR_ARCHITECTURE%
+set exdir=%deploydir%\staff\samples\bin
+if not EXIST %exdir% mkdir %exdir%
+xcopy /Y /S %target%\*.exe %exdir%
