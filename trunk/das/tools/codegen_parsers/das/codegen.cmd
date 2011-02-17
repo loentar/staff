c3531592028caffcefba6e1d6d427075b18f4ce8
loentar <loentar@46775792-f812-11dd-b507-8b4ecd5210be>
2011-02-17 07:05:56 +0000

    Fixed building DAS with MSVC

--- trunk/das/tools/codegen_parsers/das/codegen.cmd
+++ trunk/das/tools/codegen_parsers/das/codegen.cmd
@@ -1,6 +1,6 @@
 setlocal
 SET STAFF_HOME=%cd%\..\..\..\..\staff\deploy\win_%PROCESSOR_ARCHITECTURE%\staff
-set PATH=..\..\..\..\rise\deploy\win_%PROCESSOR_ARCHITECTURE%\lib;..\..\..\..\deploy\win_%PROCESSOR_ARCHITECTURE%\bin;%STAFF_HOME%\bin;%PATH%
+set PATH=%cd%\..\..\..\..\rise\deploy\win_%PROCESSOR_ARCHITECTURE%\lib;%cd%\..\..\..\deploy\win_%PROCESSOR_ARCHITECTURE%\bin;%STAFF_HOME%\bin;%PATH%
 cd src
 staff_codegen -u -tclient DataAccessService.h
 endlocal
